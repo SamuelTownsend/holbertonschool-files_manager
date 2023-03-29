f96e65bd87749c0b5fde1f1130d6fc41c3fb04c6
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-03-29 13:56:14 -0500

    adding controllers and routes again

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+
+// Create a new router
+const router = express.Router();
+// GETs
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
+// POSTs
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+// PUTs
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+module.exports = router;
